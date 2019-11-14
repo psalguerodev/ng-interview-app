f9bbfd6e7df378abfb53419a18faf4c7793b5081
Patrick Salguero <patrick.salguero.avalos@gmail.com>
2019-11-14 11:10:00 -0500

    feat(structure): Added basics initial components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { InterviewsComponent } from './pages/interviews/interviews.component';
+import { QuestionsComponent } from './pages/questions/questions.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { component: DashboardComponent, path: 'dashboard' },
+  { component: InterviewsComponent, path: 'interviews' },
+  { component: QuestionsComponent, path: 'questions' },
+  { path: '**', redirectTo: 'dashboard', pathMatch: 'full'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { InterviewsComponent } from './pages/interviews/interviews.component';
+import { QuestionsComponent } from './pages/questions/questions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    InterviewsComponent,
+    QuestionsComponent,
   ],
   imports: [
     BrowserModule,
